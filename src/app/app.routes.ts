c3dd927b86a3b3a2707276e725419f45a2dab7b3
Jose Miguel <josemiguel.cabrera312@gmail.com>
2024-01-17 08:16:01 +0100

    Errores y home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { MinionComponent } from './minion/minion.component';
 import { FocusMinionComponent } from './focus-minion/focus-minion.component';
 import { FormMinionComponent } from './form-minion/form-minion.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorsComponent } from './errors/errors.component';
 
 export const routes: Routes = [
-    {path: "", component: MinionComponent},
+    {path: "", component: HomeComponent},
     {path:"minion", component:MinionComponent,children:[{path:'focus/:id', component:FocusMinionComponent}]},
     {path:"minion/:name", component:MinionComponent},
     {path:"add/minion", component:FormMinionComponent},
-    {path:"edit/minion/:id", component:FormMinionComponent}];
+    {path:"edit/minion/:id", component:FormMinionComponent},
+    {path:"**", component:ErrorsComponent}];
